911d3b0ec717478161350f4aa11ae7930ded5e0d
AlexJ2598 <alexis.hernandez074@gmail.com>
2023-09-16 20:54:19 -0600

    Creacion de Proyecto

---
+++ ventasBackend/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ventasBackend
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ventasBackend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ventasBackend.MvcApplication" Language="C#" %>
