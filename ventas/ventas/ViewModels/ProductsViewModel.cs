2e997677621c75ba94b2faee8365cdd73bb73df0
AlexJ2598 <alexis.hernandez074@gmail.com>
2023-09-19 11:36:36 -0600

    Refresco de pantalla
    Se refresca la pantalla de productos

--- ventas/ventas/ViewModels/ProductsViewModel.cs
+++ ventas/ventas/ViewModels/ProductsViewModel.cs
@@ -1,8 +1,10 @@
 ﻿namespace ventas.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
     using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.Windows.Input;
     using ventas.Common.Models;
     using ventas.Services;
     using Xamarin.Forms;
@@ -10,11 +12,18 @@
     public class ProductsViewModel : BaseViewModel
     {
         private ApiService apiService;
+        private bool isRefreshing;
+
         private ObservableCollection<Product> products;
         public ObservableCollection<Product> Products 
         {
             get { return this.products; }
             set {this.SetValue(ref this.products, value); } 
+        }
+        public bool IsRefreshing
+        {
+            get { return this.isRefreshing; }
+            set { this.SetValue(ref this.isRefreshing, value); }
         }
         //Cargamos la lista de prodcutos
         public ProductsViewModel()
@@ -25,14 +34,26 @@
 
         private async void LoadProducts()
         {
+            this.IsRefreshing = true;
+            //var url = Application.Current.Resources["UrlAPI"].ToString(); //Consumimos el recuros url creado en APP.xaml. Necesita tiempo
             var response = await this.apiService.GetList<Product>("https://ventasapi20230917000719.azurewebsites.net", "/api", "/Products"); //Consume la url base, el prefijo y controlador
             if(!response.isSuccess)
             {
+                this.IsRefreshing = false;
                 await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Aceptar");
                 return;
             }
             var list = (List<Product>)response.Result; //Casteamos.
             this.Products = new ObservableCollection<Product>(list);
+            this.IsRefreshing = false;
+        }
+
+        public ICommand RefreshCommand //Es una propiedad solo de lecutura
+        { 
+            get
+            {
+                return new RelayCommand(LoadProducts); //Va a volver a llamar al metodo load para refrescar los productos. GalaSoft.MvvmLight.Command
+            }
         }
     }
 }
